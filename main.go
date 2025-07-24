d18fcfcc162a150a50e60a182a477ed8cb4a5601
matheus jesus <matheuss.jesus2017@gmail.com>
2025-07-24 02:54:28 -0300

    feat: set parsing flags (CLI)

--- main.go
+++ main.go
@@ -22,6 +22,29 @@ func main() {
 	flag.BoolVar(&outputJson, "json", false, "Imprimir saída em JSON")
 	flag.Parse()
 
+	fmt.Println("Programa de Benchmarking de Serviços HTTP")
+	fmt.Println("-----------------------------")
+
 	fmt.Printf("URL: %s\nTotal de requisições: %d\nConcorrência: %d\nTimeout: %s\nSaída em JSON: %v\n",
 		url, total, concurrency, timeout, outputJson)
+
+	fmt.Println("-----------------------------")
+
+	if url == "" || total <= 0 || concurrency <= 0 {
+		fmt.Println("Argumentos inválidos:")
+		if url == "" {
+			fmt.Println("- URL não pode ser vazia.")
+		}
+		if total <= 0 {
+			fmt.Println("- Total de requisições deve ser maior que zero.")
+		}
+		if concurrency <= 0 {
+			fmt.Println("- Concorrência deve ser maior que zero.")
+		}
+		return
+	}
+
+	if concurrency > total {
+		concurrency = total
+	}
 }
